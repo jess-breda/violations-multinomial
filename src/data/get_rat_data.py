0e97256b493f7bb98545b82e5cac465debeeccb9
jess-breda <jbreda19@gmail.com>
2023-10-05 18:13:55 -0400

    added in full dataset load in option and added to git ignore

--- src/data/get_rat_data.py
+++ src/data/get_rat_data.py
@@ -55,10 +55,55 @@ def get_rat_viol_data(animal_ids=None, mode="local"):
     rat_df = pd.read_csv(data_path / file_name)
 
     if animal_ids:
-        print(f"returning data for {animal_ids}")
+        print(f"returning viol data for {animal_ids}")
         rat_df = rat_df.query("animal_id == @animal_ids").copy()
         rat_df.reset_index(drop=True, inplace=True)
     else:
-        print(f"returning dataset for all animals")
+        print(f"returning viol dataset for all animals")
+
+    return rat_df
+
+
+def get_rat_data(animal_ids=None, mode="local"):
+    """
+    Function to load all rat data from the main PWM dataset
+    without truncate at end of violations being tracked.
+
+    This is similar to the raw "rat_behavior.csv() with some
+    additional formating:
+
+        subject_id -> animal_id
+        violation column added
+        n_trials column added
+        training_stage_cat column added
+        delay column is rounded to 2 decimal places
+
+    params
+    ------
+    animal_ids: str or list (default: None)
+        animal ids to return data for, will return
+        all data if None
+    mode: str (default: "local")
+        "local" or "cup" depending on where the data is stored
+        note "cup" requires a VPN to princeton network
+
+    returns
+    -------
+    rat_df pd.DataFrame
+        Dataframe containing data for specified animal(s)
+        from main PWM dataset for all trials
+    """
+
+    data_path = determine_data_path(mode=mode)
+    file_name = "processed/all_data.csv"
+
+    rat_df = pd.read_csv(data_path / file_name)
+
+    if animal_ids:
+        print(f"returning full data for {animal_ids}")
+        rat_df = rat_df.query("animal_id == @animal_ids").copy()
+        rat_df.reset_index(drop=True, inplace=True)
+    else:
+        print(f"returning full dataset for all animals")
 
     return rat_df
